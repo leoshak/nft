a0a6d01f292ac3e9515edba6479b19121621d9bb
shakshared <leoshakshared@gmail.com>
2022-03-06 13:41:52 +0530

    Create Opensea_nft.sol

---
+++ Opensea_nft.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.8.0;
+
+//Import ERC1155 token contract from Openzeppelin
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
+
+contract NFTContract is ERC1155 {
+
+    uint256 public constant ARTWORK = 0;
+    uint256 public constant PHOTO = 1;
+
+    constructor() ERC1155("") {
+        _mint(msg.sender, ARTWORK, 1, "")
+        _mint(msg.sender, PHOTO, 2, "")
+    }
+
+}
