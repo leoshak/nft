4ddc5f95a24d13bbc77397d67c7dc2fed7f21333
shakshared <leoshakshared@gmail.com>
2022-03-06 13:43:34 +0530

    Update Opensea_nft.sol

--- Opensea_nft.sol
+++ Opensea_nft.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.0;
 //Import ERC1155 token contract from Openzeppelin
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC1155/ERC1155.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 contract NFTContract is ERC1155 {
 
@@ -14,4 +15,7 @@ contract NFTContract is ERC1155 {
         _mint(msg.sender, PHOTO, 2, "")
     }
 
+    function mint(address account, uint256 id, uint256 amount) public onlyOwner {
+        _mint(account, id, amount);
+    }
 }
